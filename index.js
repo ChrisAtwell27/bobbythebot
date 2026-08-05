d4a8212b21f3257c17ca605568dc5816a67b66f3
Christopher Atwell <catwell@mys3tech.com>
2026-08-05 10:23:35 -0400

    Update index.js

--- index.js
+++ index.js
@@ -71,7 +71,7 @@ const client = new Client({
   ],
   partials: [Partials.Message, Partials.Channel, Partials.Reaction],
 });
-
+//
 // Discord client error handlers
 client.on("error", (error) => {
   console.error("[DISCORD] Client error:", error);
